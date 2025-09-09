8bb2ab378d874c330f0403154d2eda261bcebf0e
Anthony Fox <anthonyfox@Mac.fios-router.home>
2025-09-08 22:41:07 -0400

    updated manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,12 +13,7 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: "/logo.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/logo.png",
-        sizes: "512x512",
+        sizes: "any",
         type: "image/png",
       },
     ],
