21255cb5685b8a90c703d212b87a673ea3a3eb30
Anthony Fox <anthonyfox@Mac.fios-router.home>
2025-09-16 18:58:26 -0400

    added flag to get users owned playlists instead of all of the playlists in a users library

--- app/api/spotify/user/playlists/route.ts
+++ app/api/spotify/user/playlists/route.ts
@@ -2,13 +2,14 @@ import { NextResponse, type NextRequest } from "next/server";
 import getClerkOAuthToken from "utils/clerk";
 
 export async function GET(request: NextRequest) {
-  const { userId, token } = await getClerkOAuthToken();
+  const { userId, token, spotifyUserId } = await getClerkOAuthToken();
 
   if (!userId) return new Response("Unauthorized", { status: 401 });
 
   const searchParams = request.nextUrl.searchParams;
   const offset = searchParams.get("offset");
   const limit = searchParams.get("limit");
+  const ownedOnly = searchParams.get("owned_only");
   try {
     const spotifyUrl = `${process.env.BASE_SPOTIFY_URL}/me/playlists?offset=${
       offset || 0
@@ -20,6 +21,16 @@ export async function GET(request: NextRequest) {
       },
     });
     const data = await spotifyResponse.json();
+
+    if (ownedOnly) {
+      console.log(data.items[0].owner, spotifyUserId);
+
+      return NextResponse.json([
+        ...data.items.filter(
+          (playlist: any) => playlist.owner.id === spotifyUserId
+        ),
+      ]);
+    }
 
     return NextResponse.json([...data.items]);
   } catch (error) {
